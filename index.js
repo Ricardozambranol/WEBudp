db51595e366423bbbeba4a5f14630c78e6158c83
Ricardozambranol <143974781+Ricardozambranol@users.noreply.github.com>
2023-09-08 20:24:49 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,71 +1,34 @@
 const express = require('express');
 const http = require('http');
-const mysql = require('mysql2');
 const path = require('path');
+const dgram = require('dgram');
+const WebSocket = require('ws');
 
 const app = express();
 const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
-const conexionDB = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Rz_1002154588',
-  database: 'diseno_bd',
+const udpServer = dgram.createSocket('udp4');
+
+udpServer.on('error', (err) => {
+  console.error(`Error en el servidor UDP: ${err.stack}`);
+  udpServer.close();
 });
 
-conexionDB.connect((error) => {
-  if (error) {
-    console.error('Error al conectar a la base de datos:', error);
-    return;
-  }
-  console.log('Conexión a la base de datos exitosa.');
+udpServer.on('message', (message, remote) => {
+  const data = message.toString('utf8');
+  // Transmitir los datos UDP a todos los clientes WebSocket conectados
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
 });
 
-let lastSentMessage = null;
+udpServer.bind(25565); // Puerto UDP 25565
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.get('/data.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'data.html'));
-});
-
-app.get('/alldata', (req, res) => {
-  conexionDB.query('SELECT * FROM mensajes', (error, resultados) => {
-    if (error) {
-      console.error('Error al obtener datos:', error);
-      res.status(500).send('Error al obtener datos');
-      return;
-    }
-    res.json(resultados);
-  });
-});
-
-app.get('/events', (req, res) => {
-  res.setHeader('Content-Type', 'text/event-stream');
-  res.setHeader('Cache-Control', 'no-cache');
-  res.setHeader('Connection', 'keep-alive');
-  
-  const interval = setInterval(() => {
-    conexionDB.query('SELECT remitente, mensaje FROM mensajes ORDER BY id DESC LIMIT 1', (error, resultados) => {
-      if (error) {
-        console.error('Error al obtener datos:', error);
-        return;
-      }
-      if (resultados.length > 0) {
-        const data = JSON.stringify(resultados[0]);
-        if (data !== lastSentMessage) {
-          res.write(`data: ${data}\n\n`);
-          lastSentMessage = data;
-        }
-      }
-    });
-  }, 1);
-
-  req.on('close', () => {
-    clearInterval(interval);
-  });
 });
 
 const puerto = 3000;
