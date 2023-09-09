5582a1f15add53af95fd1f2f50eacd1138fe70b9
Ricardozambranol <143974781+Ricardozambranol@users.noreply.github.com>
2023-09-08 23:07:31 -0500

    Add files via upload

--- udp-listener.js
+++ udp-listener.js
@@ -2,6 +2,9 @@ const dgram = require('dgram');
 
 const udpServer = dgram.createSocket('udp4');
 
+// Importa la conexión a la base de datos desde index.js
+const { conexionDB } = require('./index');
+
 udpServer.on('error', (err) => {
   console.error(`Error en el servidor UDP: ${err.stack}`);
   udpServer.close();
@@ -10,6 +13,22 @@ udpServer.on('error', (err) => {
 udpServer.on('message', (message, remote) => {
   const data = message.toString('utf8');
   console.log(`Mensaje UDP recibido: ${data} from ${remote.address}:${remote.port}`);
+
+  // Guarda el mensaje en la base de datos
+  const insertQuery = `
+    INSERT INTO mensajes (remitente, mensaje) VALUES (?, ?)
+  `;
+  
+  const remitente = remote.address;
+  const mensaje = data;
+
+  conexionDB.query(insertQuery, [remitente, mensaje], (error) => {
+    if (error) {
+      console.error('Error al guardar el mensaje en la base de datos:', error);
+    } else {
+      console.log('Mensaje guardado en la base de datos:', mensaje);
+    }
+  });
 });
 
 udpServer.bind(25565); // Puerto UDP 25565
